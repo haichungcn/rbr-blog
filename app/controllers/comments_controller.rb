3e2386ab9c23da91a15cba31df01d3d1e62de60a
Hai Chung <hai.chung@hubble.sg>
2020-07-27 14:47:04 +0700

    working on comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,8 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  has_many :comments
+  validates :title, presence: true,
+                  length: { minimum: 5 }
 end
+
