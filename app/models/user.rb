34c9aa5bedad8a26792a16071a9eb23432dcdbd2
haichungcn <haichungcn@gmail.com>
2020-07-29 16:00:37 +0700

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :comments, dependent: :destroy
+
+  validates :name, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 25}
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
