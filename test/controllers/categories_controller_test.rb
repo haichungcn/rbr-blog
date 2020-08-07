0a2d9403f55f387b065abd0fe118e8e62fa8dcfd
Hai Chung <hai.chung@hubble.sg>
2020-08-07 16:38:45 +0700

    integration testing for new category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,21 +2,21 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: "sports")
+    @category = Category.create(name: 'sports')
     print @category.id
   end
 
-  test "should get categories index" do
+  test 'should get categories index' do
     get categories_path
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_category_path
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get category_path(@category)
     assert_response :success
   end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
-
-  test "get new category form and create category" do
+  test 'get new category form and create category' do
     get new_category_path
     assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: 'sports' } }
+      follow_redirect!
+    end
+    assert_template 'categories/index'
+    assert_match 'sports', response.body
+  end
+
+  test 'invalid category submission results in failure' do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: ' ' } }
+    end
+    assert_template 'categories/new'
+    assert_select 'p.h4.alert-title'
+    assert_select 'div.alert-body'
   end
 end
