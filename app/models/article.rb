1832cced868e6b3c99031b5fee557f4dcad03419
Hai Chung <hai.chung@hubble.sg>
2020-08-09 17:17:24 +0700

    added article_categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
-                  length: { minimum: 5, maximum: 100 }
+                    length: { minimum: 5, maximum: 100 }
   validates :user_id, presence: true
 end
-
