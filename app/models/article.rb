20c9741420ae5ec68bfb7e96075ed1b7d568d0b0
Hai Chung <hai.chung@hubble.sg>
2020-08-09 17:36:58 +0700

    fixed model name

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :article_categories
-  has_many :categories, through: :article_categories
+  has_many :categories, through: :article_categories, dependent: :destroy
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5, maximum: 100 }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :article_categories
-  has_many :articles, through: :article_categories
+  has_many :articles, through: :article_categories, dependent: :destroy
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
